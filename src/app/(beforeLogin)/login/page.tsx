f698ba2479bee3371149ef6c287ec500824798cb
flashrifle <flashrifle@gmail.com>
2025-01-03 17:01:13 +0900

    STD: 로그인 모달 로직 수정

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,19 @@
-import {redirect} from "next/navigation";
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Main from '@/app/(beforeLogin)/_component/Main';
 
 export default function Login() {
-  redirect('/i/flow/login')
+  const router = useRouter();
+  router.replace('i/flow/login');
+  return <Main />;
 }
+
+// router.push
+// localhost:3000 -> localhost:3000/login -> localhost:3000/i/flow/login
+
+// router.replace
+// localhost:3000 -> localhost:3000/login -> localhost:3000/i/flow/login
+
+// push 는 히스토리가 남아 redirect 된 이전 페이지로 가려고 하고
+// replace 는 히스토리가 남지않아 redirect 된 페이지로 가지않음 상황에 맞게 잘 사용하자.
