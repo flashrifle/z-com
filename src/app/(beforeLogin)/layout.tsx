1e1a019c53d6d776345c2e876a5ace4292fecf94
flashrifle <flashrifle@gmail.com>
2025-01-03 15:42:50 +0900

    STD: 클라이언트 컴포넌트로 전환

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -5,7 +5,6 @@ type Props = { children: ReactNode; modal: ReactNode };
 export default function Layout({ children, modal }: Props) {
   return (
     <div className={styles.container}>
-      비포로그인 레이아웃
       {children}
       {modal}
     </div>
