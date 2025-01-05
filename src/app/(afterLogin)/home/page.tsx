7d5e2fb26451fc7f9151b984823a65661bfd1d9f
flashrifle <flashrifle@gmail.com>
2025-01-05 18:19:46 +0900

    STD: 홈 탭 만들면서 Context API 적용해보기

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,17 @@
+import style from './home.module.css';
+import Tab from '@/app/(afterLogin)/home/_component/Tab';
+import PostForm from '@/app/(afterLogin)/home/_component/PostForm';
+import Post from '@/app/(afterLogin)/_component/Post';
+import TabProvider from '@/app/(afterLogin)/home/_component/TabProvider';
+
 export default function Home() {
   return (
-    <main>
-      홈페이지
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+      </TabProvider>
     </main>
-  )
+  );
 }
