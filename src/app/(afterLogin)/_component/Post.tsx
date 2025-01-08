0ab4d9acb4c4075afce94baa1b9ca6ee67e8795e
flashrifle <flashrifle@gmail.com>
2025-01-08 17:59:43 +0900

    STD: 다중 이미지 구역 만들기

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -6,6 +6,7 @@ import 'dayjs/locale/ko';
 import ActionButtons from '@/app/(afterLogin)/_component/ActionButtons';
 import PostArticle from '@/app/(afterLogin)/_component/PostArticle';
 import { faker } from '@faker-js/faker';
+import PostImages from '@/app/(afterLogin)/_component/PostImages';
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime);
@@ -28,14 +29,26 @@ export default function Post({ noImage }: Props) {
   };
 
   if (Math.random() > 0.5 && !noImage) {
-    target.Images.push({
-      imageId: 1,
-      link: faker.image.urlLoremFlickr(),
-    });
+    target.Images.push(
+      {
+        imageId: 1,
+        link: faker.image.urlLoremFlickr(),
+      },
+      {
+        imageId: 2,
+        link: faker.image.urlLoremFlickr(),
+      },
+      {
+        imageId: 3,
+        link: faker.image.urlLoremFlickr(),
+      },
+      {
+        imageId: 4,
+        link: faker.image.urlLoremFlickr(),
+      },
+    );
   }
-  if (target.Images && target.Images.length > 0) {
-    console.log(1);
-  }
+
   return (
     <PostArticle post={target}>
       <div className={style.postWrapper}>
@@ -56,15 +69,8 @@ export default function Post({ noImage }: Props) {
             <span className={style.postDate}>{dayjs(target.createdAt).fromNow(true)}</span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}>
-            {target.Images && target.Images.length > 0 && (
-              <Link
-                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
-                className={style.postImageSection}
-              >
-                <img src={target.Images[0]?.link} alt="" />
-              </Link>
-            )}
+          <div>
+            <PostImages post={target} />
           </div>
           <ActionButtons />
         </div>
