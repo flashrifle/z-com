c42e7000d90e6025b5c83a9ddc721326b12d80a9
flashrifle <flashrifle@gmail.com>
2025-02-14 16:57:56 +0900

    STD: react-query SSR 설정

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -3,15 +3,36 @@ import Tab from '@/app/(afterLogin)/home/_component/Tab';
 import PostForm from '@/app/(afterLogin)/home/_component/PostForm';
 import Post from '@/app/(afterLogin)/_component/Post';
 import TabProvider from '@/app/(afterLogin)/home/_component/TabProvider';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
-export default function Home() {
+async function getPostRecommends() {
+  const res = await fetch(`http://localhost:9090/api/postRecommends`, {
+    next: {
+      tags: ['posts', 'recommends'],
+    },
+    cache: 'no-store',
+  });
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({ queryKey: ['posts', 'recommends'], queryFn: getPostRecommends });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <main className={style.main}>
-      <TabProvider>
-        <Tab />
-        <PostForm />
-        <Post />
-      </TabProvider>
+      <HydrationBoundary state={dehydratedState}>
+        <TabProvider>
+          <Tab />
+          <PostForm />
+          <Post />
+        </TabProvider>
+      </HydrationBoundary>
     </main>
   );
 }
