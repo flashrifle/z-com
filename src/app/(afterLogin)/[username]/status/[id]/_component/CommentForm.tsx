2673dde3e6062a636a75976f6e3de440a44fbec8
flashrifle <flashrifle@gmail.com>
2025-02-20 18:51:39 +0900

    STD: 게시글 상세페이지, 답글, 포토모달

--- src/app/(afterLogin)/[username]/status/[id]/_component/CommentForm.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/_component/CommentForm.tsx
@@ -1,7 +1,13 @@
 'use client';
 import { useRef, useState } from 'react';
 import style from './commentForm.module.css';
-export default function CommentForm() {
+import { useQueryClient } from '@tanstack/react-query';
+
+type Props = {
+  id: string;
+};
+
+export default function CommentForm({ id }: Props) {
   const [content, setContent] = useState('');
   const imageRef = useRef<HTMLInputElement>(null);
   const onClickButton = () => {};
@@ -11,6 +17,14 @@ export default function CommentForm() {
     id: 'flashrifle',
     image: '/5Udwvqim.jpg',
   };
+
+  const queryClient = useQueryClient();
+  const post = queryClient.getQueryData(['posts', id]);
+
+  if (!post) {
+    return null;
+  }
+
   return (
     <form className={style.postForm} onSubmit={onSubmit}>
       <div className={style.postUserSection}>
--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,27 +1,36 @@
 import BackButton from '@/app/(afterLogin)/_component/BackButton';
 import style from './singlePost.module.css';
-import Post from '@/app/(afterLogin)/_component/Post';
 import CommentForm from '@/app/(afterLogin)/[username]/status/[id]/_component/CommentForm';
-export default function SinglePost() {
+import SinglePost from '@/app/(afterLogin)/[username]/status/[id]/_component/SinglePost';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import Comments from '@/app/(afterLogin)/[username]/status/[id]/_component/Comments';
+import { getSinglePost } from '@/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePost';
+import { getComments } from '@/app/(afterLogin)/[username]/status/[id]/_lib/getComments';
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function Page(props: Props) {
+  const { id } = await props.params;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({ queryKey: ['posts', id], queryFn: getSinglePost });
+  await queryClient.prefetchQuery({ queryKey: ['posts', id, 'comments'], queryFn: getComments });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <div className={style.main}>
-      <div className={style.header}>
-        <BackButton />
-        <h3 className={style.headerTitle}>게시하기</h3>
-      </div>
-      <Post />
-      <CommentForm />
-      <div>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </div>
+      <HydrationBoundary state={dehydratedState}>
+        <div className={style.header}>
+          <BackButton />
+          <h3 className={style.headerTitle}>게시하기</h3>
+        </div>
+        <SinglePost id={id} />
+        <CommentForm />
+        <div>
+          <Comments id={id} />
+        </div>
+      </HydrationBoundary>
     </div>
   );
 }
