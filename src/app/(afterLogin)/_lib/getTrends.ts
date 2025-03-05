aeae74cdb6f7f722701332180b466f12eed7e997
flashrifle <flashrifle@gmail.com>
2025-03-05 17:55:19 +0900

    STD: 업로드 이미지 미리보기

--- src/app/(afterLogin)/_lib/getTrends.ts
+++ src/app/(afterLogin)/_lib/getTrends.ts
@@ -1,8 +1,9 @@
 export async function getTrends() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/trends`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/hashtags/trends`, {
     next: {
       tags: ['trends'],
     },
+    credentials: 'include',
   });
   if (!res.ok) {
     throw new Error('Failed to fetch data');
